f3b2c44e6f33ea80c4bc9fdaa09f15c25eda094a
Jakub Stefan <kejkun1992@gmail.com>
2018-08-11 17:06:33 +0200

    web app root

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello() -> str:
-    return 'aaaabaa'
+def sign_up() -> 'html':
+    '''sign up form'''
+    return render_template('signup.html', the_title='Rejestracja')
+
+
+@app.route('/signIn')
+def sign_in() -> 'html':
+    '''sign in form'''
+    return render_template('signin.html', the_title='Logowanie')
+
+
+@app.route('/game', methods=['POST'])
+def game() -> 'html':
+    '''main game window'''
+    return render_template('game.html', the_title='Gramy')
 
 if __name__ == '__main__':
     app.run(debug=True)
